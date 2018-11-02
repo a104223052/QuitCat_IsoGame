f1d09f4f9ff532835320cbaab5169885940c7e7c
Gary 李 <Gary@garydemacbook-pro.local>
2018-11-02 23:01:02 +0800

    add record view 1/4 in TestGameViewController

--- QuitCatIsoGame/Game/TestGame/TestGameViewController.swift
+++ QuitCatIsoGame/Game/TestGame/TestGameViewController.swift
@@ -12,6 +12,8 @@ class TestGameViewController: UIViewController {
 
     @IBOutlet weak var myGifView: UIImageView!
     @IBOutlet var recordView: UIView!
+    @IBOutlet weak var feedView: UIView!
+    @IBOutlet var feedCompleteView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,8 +21,25 @@ class TestGameViewController: UIViewController {
         recordView.layer.borderColor = UIColor.white.cgColor
         
         myGifView.loadGif(name: "standtest_0")
+        feedView.layer.cornerRadius = 10
         
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.addSubview(feedCompleteView)
+        feedCompleteView.translatesAutoresizingMaskIntoConstraints = false
+        
+//        feedCompleteView.heightAnchor.constraint(equalToConstant: 128).isActive = true
+//        feedCompleteView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+//        feedCompleteView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        let c = feedCompleteView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -128)
+        c.identifier = "pickerViewBottom"
+        c.isActive = true
+        
+        feedView.layer.cornerRadius = 10
+        
+        super.viewWillAppear(animated)
     }
     
     
