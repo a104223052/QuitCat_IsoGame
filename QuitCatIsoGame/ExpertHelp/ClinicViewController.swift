41e31bca93c363a88cc5aa48b42545126efe0441
Gary 李 <Gary@garydemacbook-pro.local>
2018-11-15 03:20:52 +0800

    clinic complete

--- QuitCatIsoGame/ExpertHelp/ClinicViewController.swift
+++ QuitCatIsoGame/ExpertHelp/ClinicViewController.swift
@@ -12,6 +12,7 @@ import CSVImporter
 
 class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPickerViewDelegate {
     
+    
     var gridViewController: UICollectionGridViewController!
     var local_localtion:String = ""
     var name = [String]()
@@ -21,10 +22,21 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
     var guardian = [String]()
     
     let cityList = ["臺北市" , "新北市" , "桃園市" , "新竹市" , "苗栗縣"  , "台中市"  ]
-    let regionList = ["新店區" , "永和區" , "萬里區" , "金山區" , "板橋區"  , "瑞芳區"]
+    let nullList = ["123","321 "]
+    
+    let taipeiList = ["中正區" , "萬華區" , "大同區" , "中山區" , "松山區", "大安區",
+                      "信義區" , "內湖區",  "士林區" , "北投區" , "文山區"]
+    
+    let regionList = [ "萬里區","金山區","板橋區","汐止區","深坑區",
+                       "石碇區","瑞芳區","平溪區","雙溪區","貢寮區",
+                       "新店區","坪林區","烏來區","永和區","中和區",
+                       "土城區","三峽區","樹林區","鶯歌區","三重區",
+                       "新莊區","泰山區","林口區","蘆洲區","五股區",
+                       "八里區","淡水區","三芝區","石門區"]
     var citySelect = "新北市"
-    var regionSelect = "新店區"
+    var regionSelect = ""
     var cityregionSelect = "新北市新店區"
+    var isCitySelect:Bool = false
     
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var regionButton: UIButton!
@@ -42,57 +54,62 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         gridViewController.setColumns(columns: ["名稱", "電話", "給藥", "衛教" ])
         view.addSubview(gridViewController.view)
         gridViewController.view.removeFromSuperview()
-
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        view.addSubview(cityView)
-        view.addSubview(regionView)
-        cityView.translatesAutoresizingMaskIntoConstraints = false
-        regionView.translatesAutoresizingMaskIntoConstraints=false
-        cityView.heightAnchor.constraint(equalToConstant: 128).isActive = true
-        regionView.heightAnchor.constraint(equalToConstant: 128).isActive = true
         
-        cityView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive=true
-        regionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive=true
-        
-        cityView.trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -10).isActive = true
-        regionView.trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -10).isActive = true
-
-        let city = cityView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 128)
-        let region = regionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 128)
-        
-        city.identifier = "bottom"
-        region.identifier = "bottombottom"
-        
-        city.isActive = true
-        region.isActive = true
-        
-        cityView.layer.cornerRadius = 10
-        regionView.layer.cornerRadius = 10
+        if(isCitySelect == true){
+            view.addSubview(regionView)
+            regionView.translatesAutoresizingMaskIntoConstraints=false
+            regionView.heightAnchor.constraint(equalToConstant: 160).isActive = true
+            regionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive=true
+            regionView.trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -10).isActive = true
+            let region = regionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 160)
+            region.identifier = "bottombottom"
+            region.isActive = true
+            regionView.layer.cornerRadius = 10
+            
+        }else{
+            view.addSubview(cityView)
+            cityView.translatesAutoresizingMaskIntoConstraints = false
+            cityView.heightAnchor.constraint(equalToConstant: 128).isActive = true
+            cityView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive=true
+            cityView.trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -10).isActive = true
+            let city = cityView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 128)
+            city.identifier = "bottom"
+            city.isActive = true
+            cityView.layer.cornerRadius = 10
+        }
         super.viewWillAppear(animated)
         
     }
     
     
     @IBAction func showClinic(_ sender: Any) {
-        sessionSimpleDownload()
+        //sessionSimpleDownload()
+        
+        gridViewController.view.removeFromSuperview()
+        
         gridViewController = UICollectionGridViewController()
+        
         gridViewController.setColumns(columns: ["名稱", "地圖"  ,"電話", "給藥", "衛教" ])
+        gridViewController.setData(nameA: name, phoneA:phone, addressA: address,
+                                   administrationA: administration, guardianA: guardian)
         for (index,element) in name.enumerated() {
-            gridViewController.addRow(row: [element , "123"  , phone[index] , administration[index] , guardian[index]] )
+            gridViewController.addRow(row: [element , "map"  , "phone" , administration[index] , guardian[index]] )
         }
         view.addSubview(gridViewController.view)
-        
     }
     
-     override func viewDidLayoutSubviews() {
-     gridViewController.view.frame = CGRect(x:0, y:320, width:view.frame.width,
-     height:400)
-     }
-     override func didReceiveMemoryWarning() {
-     super.didReceiveMemoryWarning()
-     }
+    override func viewDidLayoutSubviews() {
+        gridViewController.view.frame = CGRect(x:0, y:320, width:view.frame.width,
+                                               height:400)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     
     
@@ -109,12 +126,16 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         }
         
     }
-
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView.tag == 1{
             return cityList.count
         }else{
-            return regionList.count
+            if (citySelect == "臺北市"){
+                return taipeiList.count
+            }else if(citySelect == "新北市"){
+                return regionList.count
+            }
+            return nullList.count
         }
         
     }
@@ -123,7 +144,13 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         if pickerView.tag == 1 {
             return cityList[row]
         }else{
-            return regionList[row]
+            if (citySelect == "臺北市"){
+                return taipeiList[row]
+            }else if(citySelect == "新北市"){
+                return regionList[row]
+            }
+            
+            return nullList[row]
         }
     }
     
@@ -131,15 +158,24 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
     
     
     @IBAction func downRegionClick(_ sender: Any) {
-        let titleRegion = regionList[pickerRegionView.selectedRow(inComponent: 0)]
+        var titleRegion = "null"
+        if(citySelect == "臺北市"){
+            titleRegion = taipeiList[pickerRegionView.selectedRow(inComponent: 0)]
+        }else if(citySelect == "新北市"){
+            titleRegion = regionList[pickerRegionView.selectedRow(inComponent: 0)]
+            
+        }
         regionButton.setTitle(titleRegion , for: .normal)
         regionSelect = titleRegion
         cityregionSelect = citySelect + regionSelect
+        print(cityregionSelect)
         readcsv( inputAddress: cityregionSelect)
         for items in address{
             print(items)
         }
+        isCitySelect = false
         displayPickerRegionView(false)
+        regionView.removeFromSuperview()
     }
     
     @IBAction func downClick(_ sender: Any) {
@@ -147,23 +183,24 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         cityButton.setTitle(titleCity , for: .normal)
         citySelect = titleCity
         displayPickerCityView(false)
+        isCitySelect = true
+        cityView.removeFromSuperview()
     }
     
     @IBAction func chooseRegion(_ sender: Any) {
+        viewWillAppear(true)
         displayPickerRegionView(true)
-
     }
     @IBAction func chooseCity(_ sender: Any) {
+        viewWillAppear(true)
         displayPickerCityView(true)
     }
-    
-    
     
     
     func displayPickerRegionView(_ show : Bool){
         for c in view.constraints{
             if c.identifier == "bottombottom"{
-                c.constant = (show) ? -10 : 128
+                c.constant = (show) ? -10 : 160
                 break
             }
         }
@@ -183,6 +220,11 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         }
     }
     func readcsv(inputAddress:String) {
+        self.name.removeAll()
+        self.phone.removeAll()
+        self.address.removeAll()
+        self.administration.removeAll()
+        self.guardian.removeAll()
         let path = local_localtion
         let importer = CSVImporter<[String]>(path: path)
         importer.startImportingRecords { $0 }.onFinish { importedRecords in
@@ -226,6 +268,27 @@ class ClinicViewController: UIViewController , UIPickerViewDataSource , UIPicker
         //使用resume方法启动任务
         downloadTask.resume()
     }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /*
+         displayPickerCityView(false)
+         displayPickerRegionView(false)
+         
+         let titleCity = cityList[pickerCityView.selectedRow(inComponent: 0)]
+         cityButton.setTitle(titleCity , for: .normal)
+         citySelect = titleCity
+         
+         let titleRegion = regionList[pickerRegionView.selectedRow(inComponent: 0)]
+         regionButton.setTitle(titleRegion , for: .normal)
+         regionSelect = titleRegion
+         cityregionSelect = citySelect + regionSelect
+         print(cityregionSelect)
+         readcsv( inputAddress: cityregionSelect)
+         for items in address{
+         print(items)
+         }*/
+    }
 }
 extension String {
     func contains(find: String) -> Bool{
@@ -235,5 +298,6 @@ extension String {
         return self.range(of: find, options: .caseInsensitive) != nil
     }
 }
+
 
 
--- QuitCatIsoGame/ExpertHelp/UICollectionGridViewController.swift
+++ QuitCatIsoGame/ExpertHelp/UICollectionGridViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 //多列表格组件（通过CollectionView实现）
 class UICollectionGridViewController: UICollectionViewController {
@@ -16,6 +17,14 @@ class UICollectionGridViewController: UICollectionViewController {
     //行数据
     var rows: [[Any]]! = []
     //单元格内容居左时的左侧内边距
+    var name = [String]()
+    var phone = [String]()
+    var address = [String]()
+    var administration = [String]()
+    var guardian = [String]()
+    
+    
+    
     private var cellPaddingLeft:CGFloat = 5
     
     init() {
@@ -40,6 +49,19 @@ class UICollectionGridViewController: UICollectionViewController {
     //设置列头数据
     func setColumns(columns: [String]) {
         cols = columns
+    }
+    
+    func setData(nameA:[String] , phoneA:[String] , addressA:[String] , administrationA:[String] ,  guardianA:[String] ){
+        name.removeAll()
+        phone.removeAll()
+        address.removeAll()
+        administration.removeAll()
+        guardian.removeAll()
+        name = nameA
+        phone = phoneA
+        address = addressA
+        administration = administrationA
+        guardian = guardianA
     }
     
     //添加行数据
@@ -102,10 +124,50 @@ class UICollectionGridViewController: UICollectionViewController {
         return cell
     }
     
+    func beginNav(_ startPlacemark: CLPlacemark, endPlacemark: CLPlacemark) {
+        let startP: MKPlacemark = MKPlacemark(placemark: startPlacemark)
+        let startItem: MKMapItem = MKMapItem(placemark: startP)
+        let endP: MKPlacemark = MKPlacemark(placemark: endPlacemark)
+        let endItem: MKMapItem = MKMapItem(placemark: endP)
+        let mapItems: [MKMapItem] = [startItem, endItem]// 起点和终点
+        let dic: [String : Any] = [// 导航设置字典
+            MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving,// 导航模式
+            MKLaunchOptionsMapTypeKey: MKMapType.standard.rawValue,// 地图样式
+            MKLaunchOptionsShowsTrafficKey: true// 显示交通
+        ]
+        MKMapItem.openMaps(with: mapItems, launchOptions: dic)
+    }
+    
+    lazy var geoCoder: CLGeocoder = {
+        return CLGeocoder()
+    }()
+    
     //单元格选中事件
     override func collectionView(_ collectionView: UICollectionView,
                                  didSelectItemAt indexPath: IndexPath) {
         //打印出点击单元格的［行,列］坐标
         print("点击单元格的[行,列]坐标: [\(indexPath.section),\(indexPath.row)]")
+        
+        if(indexPath.row == 1){
+            var addressString:String = address[(indexPath.section)-1]
+            geoCoder.geocodeAddressString("世新大學") { (pls: [CLPlacemark]?, error) -> Void in
+                let shPlacemark = pls?.first
+                self.geoCoder.geocodeAddressString(addressString) { (pls: [CLPlacemark]?, error) -> Void in
+                    let bjPlacemark = pls?.first
+                    self.beginNav(shPlacemark!, endPlacemark: bjPlacemark!)
+                }
+            }
+        }else if(indexPath.row == 2){
+            let number:String = phone[(indexPath.section)-1]
+            let result = number.replacingOccurrences(of: "-", with: "",
+                                                     options: NSString.CompareOptions.literal, range:nil)
+            let secondresuld = result.replacingOccurrences(of: " ", with: "",
+                                                           options: NSString.CompareOptions.literal, range:nil)
+            if let url = URL(string: "tel://"+secondresuld ) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        print(name[(indexPath.section)-1])
+        
     }
 }
